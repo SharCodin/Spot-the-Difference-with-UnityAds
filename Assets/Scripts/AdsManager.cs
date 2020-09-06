2fab1cebe3707040428934279d42efd59985de91
SSharWorks <SharCoding@gmail.com>
2020-09-06 22:08:19 +0400

    Created AdsManager script and deriving from IUnityAdsListener

---
+++ Assets/Scripts/AdsManager.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.Advertisements;
+
+public class AdsManager : MonoBehaviour, IUnityAdsListener
+{
+
+
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        
+    }
+
+    public void OnUnityAdsDidFinish(string placementId, ShowResult showResult)
+    {
+
+    }
+
+    public void OnUnityAdsDidStart(string placementId)
+    {
+
+    }
+
+    public void OnUnityAdsReady(string placementId)
+    {
+
+    }
+
+    public void OnUnityAdsDidError(string message)
+    {
+
+    }
+
+}
