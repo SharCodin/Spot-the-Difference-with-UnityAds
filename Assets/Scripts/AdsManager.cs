431f3c6bca6ba5ffe16cc477a986bb8289eb6a2e
SSharWorks <SharCoding@gmail.com>
2020-09-06 22:43:42 +0400

    Ads working

--- Assets/Scripts/AdsManager.cs
+++ Assets/Scripts/AdsManager.cs
@@ -1,39 +1,83 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.Advertisements;
 
 public class AdsManager : MonoBehaviour, IUnityAdsListener
 {
 
+    [SerializeField] private string gameId = "3808055";
+    [SerializeField] private bool testMode = true;
+    [SerializeField] private Text testText = null;
+    [SerializeField] private GameObject testButton = null;
+
+    string myPlacementId = "rewardedVideo";
 
 
     private void Start()
     {
-        
+        testButton.SetActive(false);
+
+        Advertisement.AddListener(this);
+        Advertisement.Initialize(gameId, testMode);
     }
 
-    private void Update()
+    public void ShowRewardedVideo()
     {
-        
+        // Check if UnityAds ready before calling Show method:
+        if (Advertisement.IsReady(myPlacementId))
+        {
+            Advertisement.Show(myPlacementId);
+        }
+        else
+        {
+            Debug.Log("Rewarded video is not ready at the moment! Please try again later!");
+        }
     }
 
+    // Implement IUnityAdsListener interface methods:
     public void OnUnityAdsDidFinish(string placementId, ShowResult showResult)
     {
-
-    }
-
-    public void OnUnityAdsDidStart(string placementId)
-    {
-
+        // Define conditional logic for each ad completion status:
+        if (showResult == ShowResult.Finished)
+        {
+            // Reward the user for watching the ad to completion.
+            testText.text = "Good job, you were rewarded.";
+        }
+        else if (showResult == ShowResult.Skipped)
+        {
+            // Do not reward the user for skipping the ad.
+        }
+        else if (showResult == ShowResult.Failed)
+        {
+            Debug.LogWarning("The ad did not finish due to an error.");
+        }
     }
 
     public void OnUnityAdsReady(string placementId)
     {
-
+        // If the ready Placement is rewarded, show the ad:
+        if (placementId == myPlacementId)
+        {
+            // Optional actions to take when the placement becomes ready(For example, enable the rewarded ads button)
+            testButton.SetActive(true);
+        }
     }
 
     public void OnUnityAdsDidError(string message)
     {
+        // Log the error.
+        Debug.Log(message);
+    }
 
+    public void OnUnityAdsDidStart(string placementId)
+    {
+        // Optional actions to take when the end-users triggers an ad.
+    }
+
+    // When the object that subscribes to ad events is destroyed, remove the listener:
+    public void OnDestroy()
+    {
+        Advertisement.RemoveListener(this);
     }
 
 }
