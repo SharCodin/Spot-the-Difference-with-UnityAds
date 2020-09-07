1fa2897a17242aba5abaf5d8efab592de367f823
SSharWorks <SharCoding@gmail.com>
2020-09-07 20:34:48 +0400

    version 1 build after verifying aspect ratio

--- Assets/Scripts/DifferenceFound.cs
+++ Assets/Scripts/DifferenceFound.cs
@@ -5,12 +5,16 @@ public class DifferenceFound : MonoBehaviour
 
     private void Start()
     {
-        
+
     }
 
     private void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("Difference found.");
+        }
+
     }
 
 }
