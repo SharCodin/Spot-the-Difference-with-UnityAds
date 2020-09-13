299506e285702aa8e0bbcb391ba58f822ab29f5a
SSharWorks <SharCoding@gmail.com>
2020-09-13 19:46:23 +0400

    Main menu: still in progress...

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+
+    public void Tutorial()
+    {
+        // Tutorial button pressed.
+        Debug.Log("Tutorial button pressed.");
+    }
+
+    public void Play()
+    {
+        SceneManager.LoadScene("Room001");
+    }
+
+    public void LoadLevel()
+    {
+        // Level selector
+        Debug.Log("Level selector");
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+        Debug.Log("Application Quit.");
+    }
+
+}
