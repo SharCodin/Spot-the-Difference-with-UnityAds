21731f02d084cdf3d40a8d00aa4588b819eb059f
SSharWorks <SharCoding@gmail.com>
2020-09-13 21:58:46 +0400

    Add: Resolution checker script, Arrow sprite. Main Menu still in progress.

---
+++ Assets/Scripts/ResolutionChecker.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class ResolutionChecker : MonoBehaviour
+{
+
+    [SerializeField] private Transform mainMenuButton = null;
+    [SerializeField] private Transform previousButton = null;
+    [SerializeField] private Transform nextButton = null;
+
+    private void Start()
+    {
+
+        Debug.Log(mainMenuButton.name, mainMenuButton);
+
+        Rect myRect = Screen.safeArea;
+        if (myRect.width < 700.0f)
+        {
+            mainMenuButton.position = new Vector3(-5.0f, 4.4f, 90.0f);
+            previousButton.position = new Vector3(-5.0f, previousButton.position.y, 90.0f);
+            nextButton.position = new Vector3(5.0f, nextButton.position.y, 90.0f);
+        }
+    }
+
+}
